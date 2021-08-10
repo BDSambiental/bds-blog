b12b90d8581b6459b1f15fc3d64c3fe0a37b1dd8
Pietro Belluno <pietrop@outlook.com>
2021-08-10 20:56:12 -0300

    initial commit

---
+++ src/components/bio.js
@@ -0,0 +1,59 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
+import styled from "styled-components"
+
+import { rhythm } from "../utils/typography"
+
+function Bio() {
+  return (
+    <StaticQuery
+      query={bioQuery}
+      render={data => {
+        const { author, social } = data.site.siteMetadata
+        return (
+          <Container>
+            <p>
+              <a href={`https://instagram.com/bds_ambiental`} target="_blank">
+                Nos siga no instagram.
+              </a>
+            </p>
+          </Container>
+        )
+      }}
+    />
+  )
+}
+
+const bioQuery = graphql`
+  query BioQuery {
+    avatar: file(absolutePath: { regex: "/gatsby-icon.png/" }) {
+      childImageSharp {
+        fixed(width: 50, height: 50) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        author
+        social {
+          twitter
+        }
+      }
+    }
+  }
+`
+
+const Container = styled.div`
+  display: flex;
+`
+
+export default Bio
